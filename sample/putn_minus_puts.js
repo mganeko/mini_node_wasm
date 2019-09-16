8dc3e9bc5cdfcb18f3e5f9cd4e9580a4eb3e3324
masashi.g <masashi.g@gmail.com>
2019-09-16 21:00:44 +0900

    add NG sample (for minus value)

---
+++ sample/putn_minus_puts.js
@@ -0,0 +1,13 @@
+putn(123);
+puts('abc');
+putn(456);
+puts('ABCDEFG');
+
+putn(0); // OK
+putn(2147483647); // OK
+//putn(-1); // NG (compile error)
+//putn(-2147483647);
+
+-1; // NG compile error
+
+1;
