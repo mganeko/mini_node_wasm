deb5013d3a1238392eed94e88a003521b50b715c
masashi.g <masashi.g@gmail.com>
2019-12-15 14:20:39 +0900

    run wasi with node v13.3

---
+++ run_wasi.js
@@ -0,0 +1,35 @@
+// run_wasi.js
+//  refer https://nodejs.org/api/wasi.html
+
+'use strict'
+
+const fs = require('fs');
+
+const filename = process.argv[2]; // 対象とするwasmファイル名
+console.warn('Loading wasm/wasi file: ' + filename);
+
+const { WASI } = require('wasi');
+const wasi = new WASI({
+  args: process.argv,
+  env: process.env,
+  preopens: {
+    //'/sandbox': '/some/real/path/that/wasm/can/access'
+  }
+});
+const importObject = { wasi_unstable: wasi.wasiImport };
+
+(async () => {
+  const wasm = await WebAssembly.compile(fs.readFileSync(filename)).catch(err => {
+    console.log(err);
+    process.exit(1);
+  });;
+  const instance = await WebAssembly.instantiate(wasm, importObject).catch(err => {
+    console.log(err);
+    process.exit(2);
+  });
+
+  wasi.start(instance);
+})();
+
+
+
